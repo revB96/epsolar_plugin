36003e2016cdff1b2ca1eded6ecaa74b41810d95
Querly96 <showpolska@gmail.com>
2022-06-12 10:40:08 +0200

    Initial

---
+++ epsolar.py
@@ -0,0 +1,13 @@
+# Epsolar Python Plugin
+#
+# Author: revB96
+#
+"""
+<plugin key="xfr_discusage" name="Epsolar usage" author="revB96" version="1.0.0" externallink="https://github.com/revB96/epsolar_plugin">
+    <params>
+        <param field="baud" label="Baud" width="200px" required="true" default="115200"/>
+        <param field="interval" label="Update interval" width="100px" required="true" default="60"/>
+    </params>
+</plugin>
+"""
+
